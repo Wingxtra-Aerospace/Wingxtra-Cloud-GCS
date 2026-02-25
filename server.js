d427bafacd6997997fdde25053e419e13850867f
esasujnr <esasujnr@gmail.com>
2026-02-25 18:37:15 +0000

    chore: switch deployment docs/config to dist output

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ https.createServer(sslOptions, app).listen(3000, () => {
   console.log('HTTPS server running on port 3000');
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
